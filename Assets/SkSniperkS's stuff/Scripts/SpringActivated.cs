ca47d040134cc757932003714eef7ee8c63a12ed
SkSniperkS <SkSniperkS@users.noreply.github.com>
2019-06-29 19:15:14 +0200

    Spring and falling animation
    All changes are inside of my file except the falling animation which was already put into the controller.

---
+++ Assets/SkSniperkS's stuff/Scripts/SpringActivated.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringActivated : MonoBehaviour {
+
+    [SerializeField] Vector3 springDistance;
+    CharacterController controller;
+
+    private void Start()
+    {
+        enabled = false;
+        springDistance = transform.TransformDirection(springDistance);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            controller = other.GetComponentInParent<CharacterController>();
+            enabled = true;
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 currentLeap = springDistance * Time.deltaTime;
+        controller.Move(currentLeap);
+    }
+
+    private void LateUpdate()
+    {
+        if (controller.isGrounded)
+        {
+            enabled = false;
+        }
+    }
+}
---
+++ Assets/SkSniperkS's stuff/Scripts/TemporaryAnimationScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemporaryAnimationScript : MonoBehaviour {
+    Animator animator;
+    CharacterController controller;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update () {
+        animator.SetBool("isGrounded", controller.isGrounded);
+	}
+}
